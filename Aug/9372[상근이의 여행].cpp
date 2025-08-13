a3d09bcd73e0a101be51d7da182519067f6af6a4
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-08-13 13:53:51 +0900

    Create 9372[상근이의 여행].cpp

---
+++ Aug/9372[상근이의 여행].cpp
@@ -0,0 +1,22 @@
+// 8/13 (수) 13:48 ~ 13:53
+// 실버4
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while(t > 0) {
+        t--;
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << n-1 << "\n";
+    }
+}
